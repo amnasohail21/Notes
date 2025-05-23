9d195d9174bc64b880f63e4dab3eab5785e784fc
Amna Sohail <amna.sohail7866@gmail.com>
2025-05-23 15:37:55 -0400

    first commit

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer, NoteSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
+
+# Create your views here.
