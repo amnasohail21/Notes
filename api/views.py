68d634d35b53405229c9f42e4049694f667e8620
Amna Sohail <amna.sohail7866@gmail.com>
2025-05-23 17:07:04 -0400

    registration view and connecting auth routes

--- api/views.py
+++ api/views.py
@@ -6,3 +6,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Note
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
